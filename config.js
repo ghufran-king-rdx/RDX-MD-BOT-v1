c12dc3aa328fc5cb54e16e071623f5935d01839d
GHUFRAN KING RDX <ghufranking08@gmail.com>
2026-08-12 02:00:28 +0500

    Remove TELEGRAM_TOKEN from configuration
    Removed the TELEGRAM_TOKEN placeholder from config.

--- config.js
+++ config.js
@@ -2,10 +2,6 @@ module.exports = {
   BOT_NAME: "RDX MD BOT",
   OWNER_NAME: "GHUFRAN KING",
   PREFIX: ".",
-
-  // Telegram BotFather token yahan baad mein add karna
-  TELEGRAM_TOKEN: "",
-
   VERSION: "1.0.0",
   MODE: "public"
 };
